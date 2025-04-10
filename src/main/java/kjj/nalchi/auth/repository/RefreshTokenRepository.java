43d55d9b3f99a94db229b6879c6f308b6ff2fb5d
jaejoong0529 <wcn07003@naver.com>
2025-04-10 18:35:02 +0900

    feat: RefreshToken 엔티티 및 레포지토리

---
+++ src/main/java/kjj/nalchi/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package kjj.nalchi.auth.repository;
+
+import kjj.nalchi.auth.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
+}
