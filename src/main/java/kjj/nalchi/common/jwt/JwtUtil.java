064669899f38ca93eb33ce1fd073614fcb4ee897
jaejoong0529 <wcn07003@naver.com>
2025-04-10 18:30:17 +0900

    feat: JwtUtil 생성

---
+++ src/main/java/kjj/nalchi/common/jwt/JwtUtil.java
@@ -0,0 +1,76 @@
+package kjj.nalchi.common.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtUtil {
+    private final long accessTokenExpiration = 1000 * 60 * 60; // 1시간
+    private final long refreshTokenExpiration = 1000 * 60 * 60 * 24 * 7; // 7일
+
+    @Value("${jwt.secret}")
+    private String secret;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secret);
+        this.key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    //Access Token 생성 (1시간)
+    public String generateAccessToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+
+    }
+
+    //Refresh Token 생성 (7일)
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    //JWT에서 사용자 이름 추출
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    //유효성 검사
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public String extractToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        return (token != null && token.startsWith("Bearer ")) ? token.substring(7) : null;
+    }
+}
